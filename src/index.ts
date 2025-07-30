c17275817997ee1939c49823489acbc8010e6dd3
ZhangYuan <zyct@vip.qq.com>
2025-07-30 15:21:46 +0800

    feat: 还是使用外网域名

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import  { ServerType, startStdioServer }  from 'mcp-proxy';
 export default async function main() {
   const server = await startStdioServer({
     serverType: ServerType.SSE,
-    url: "https://renzhihao.top:8882/sse",
-    // url: "http://47.82.120.53/sse",
-    // url: "https://mcp.leaper.ai/sse",
+    // url: "https://renzhihao.top:8882/sse",
+    url: "https://mcp.leaper.ai/sse",
   });
   return server
 }
