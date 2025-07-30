7210013dbff59ffac6e83cbeca39801ff02a56c4
ZhangYuan <zyct@vip.qq.com>
2025-07-30 11:37:28 +0800

    fix: 修复无法获取域名地址

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import  { ServerType, startStdioServer }  from 'mcp-proxy';
 async function main() {
   await startStdioServer({
     serverType: ServerType.SSE,
-    url: "https://mcp.leaper.ai/sse",
+    url: "https://renzhihao.top:8882/sse",
+    // url: "http://47.82.120.53/sse",
+    // url: "https://mcp.leaper.ai/sse",
   });
 }
 
