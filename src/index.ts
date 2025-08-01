31b4e83df6f3867a998f881595ea00ca65401222
ZhangYuan <zyct@vip.qq.com>
2025-08-01 15:35:28 +0800

    feat: 选择核心工具

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { z } from "zod";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
 import * as fs from "fs";
-import { McpSseReader } from 'read-mcp-sse';
 
 const mcpProxySseUrl = process.env.MCP_PROXY_URL || "https://mcp.leaper.ai/sse";
 
@@ -10,34 +10,25 @@ export default function main() {
     name: "Leaper Vision Toolkit",
     version: "1.0.0",
   });
-
+  let cli: Client;
   const methodsRes = JSON.parse(fs.readFileSync("./leaper-mcp-methods.json").toString());
   for(const  methods of methodsRes.tools) {
-    const properties = {}
-    for(const propertieName of Object.keys(methods.inputSchema.properties)) {
-      const propertie = methods.inputSchema.properties[propertieName];
-      if(propertie.type==='integer') {
-        propertie.type="number";
-      }
-      properties[propertieName] = z[propertie.type]().describe(propertie.description)
-    }
-    server.tool(methods.name, methods.description, properties ,async (params) => {
-      let mcpSseReader: McpSseReader;
+      server.registerTool(methods.name,methods,async (params) => {
       try {
-        // 每次调用重开会话
-        mcpSseReader = new McpSseReader(mcpProxySseUrl);
-        mcpSseReader.timeout = 30000;
-        const res =  await mcpSseReader.callMethod(methods.name, params);
-        return res;
+        cli = new Client({ name: "mcp-proxy-cli", version: "1.0.0" })
+        await cli.connect(new SSEClientTransport(new URL(mcpProxySseUrl)))
+        return await cli.callTool({
+          name: methods.name,
+          arguments: params,
+        });
       } catch (error) {
         console.error(error);
         return {
           content: [{ type: "text", text: error.message }],
         };
       } finally {
-        mcpSseReader.close();
+        await cli.close()
       }
-      
     });
   }
   return server.server;
